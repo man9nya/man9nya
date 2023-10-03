2ae5b0bf8755ec566e918fbb53feca3eab1cd25c
Anita <117078330+man9nya@users.noreply.github.com>
2023-10-03 12:56:52 +0300

    home page test

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div>
+            <h1>Kierratys</h1>
+            <p>tämä sivu on tarkoitettu kierrätyskeskusten etsimiseksi</p>
+        </div>
+        
+    )
+}
+
+export default Home
